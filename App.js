26ed9de2bfb3fc3be9b062982982ab726e259f4d
Ben Flores <beflore@microsoft.com>
2019-03-03 20:37:57 -0800

    import missing reducer

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createAppContainer } from "react-navigation";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { purple } from "./utils/colors";
+import reducer from "./reducers";
 
 const AppContainer = createAppContainer(Nav);
 
