c8860870ac9d2677b467b7f54ac5134daa28bf95
Ben Flores <beflore@microsoft.com>
2019-03-03 20:56:01 -0800

    Import and use middleware

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { purple } from "./utils/colors";
 import reducer from "./reducers";
+import middleware from "./middleware";
 
 const AppContainer = createAppContainer(Nav);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={createStore(reducer, middleware)}>
         <View style={{ flex: 1 }}>
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
           <AppContainer />
