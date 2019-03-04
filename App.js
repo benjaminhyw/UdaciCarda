91755971d08e8f5175047fdfeeb13851b185f702
Ben Flores <beflore@microsoft.com>
2019-03-03 19:32:22 -0800

    Wrap App.js in a provider

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View } from "react-native";
 import UdaciStatusBar from "./components/UdaciStatusBar";
 import Nav from "./components/Nav";
 import { createAppContainer } from "react-navigation";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { purple } from "./utils/colors";
 
 const AppContainer = createAppContainer(Nav);
@@ -10,10 +12,12 @@ const AppContainer = createAppContainer(Nav);
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-        <AppContainer />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <AppContainer />
+        </View>
+      </Provider>
     );
   }
 }
