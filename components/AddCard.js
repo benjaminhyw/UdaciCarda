54b71d536d12ae4a10ff61d01b3fe03cba7984cf
Ben Flores <beflore@microsoft.com>
2019-03-06 13:40:53 -0800

    Add basic UX for AddCard

---
+++ components/AddCard.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  StyleSheet
+} from "react-native";
+import { timeToString } from "../utils/helpers";
+import { purple, white } from "../utils/colors";
+import { connect } from "react-redux";
+import { addCard } from "../actions";
+import { submitCard } from "../utils/api";
+import { NavigationActions } from "react-navigation";
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={
+        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.androidSubmitbtn
+      }
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>Submit</Text>
+    </TouchableOpacity>
+  );
+}
+
+class AddCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: "Question",
+      answer: "Answer"
+    };
+  }
+
+  submit = () => {
+    const key = timeToString(); // this should be d${deckId}q${questionId}
+    const { question, answer } = this.state;
+
+    let card = {
+      key: key,
+      question,
+      answer
+    };
+    this.props.dispatch(addCard(card));
+    this.toHome();
+    submitCard(card);
+  };
+
+  toHome = () => {
+    this.props.navigation.dispatch(
+      NavigationActions.back({
+        key: "AddCard"
+      })
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.center}>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={question => this.setState({ question })}
+            value={this.state.question}
+          />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={answer => this.setState({ answer })}
+            value={this.state.answer}
+          />
+          <SubmitBtn onPress={this.submit} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  textInput: {
+    width: 200,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitbtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 30,
+    marginLeft: 30
+  }
+});
+
+export default connect()(AddCard);
