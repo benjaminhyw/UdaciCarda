4bb98a53386c8ee2dc9ce5ec130cf2e5cde3a879
Ben Flores <beflore@microsoft.com>
2019-03-06 17:14:05 -0800

    Implement combineReducers logic, update imports

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { createAppContainer } from "react-navigation";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { purple } from "./utils/colors";
-import reducer from "./reducers";
+import reducer from "./reducers/index";
 import middleware from "./middleware";
 
 const AppContainer = createAppContainer(Nav);
