8099ace0e6a31d87cfcd70da7051b94f1e99d873
Ben Flores <beflore@microsoft.com>
2019-03-17 20:45:35 -0700

    Add UPDATE_DECK action

--- actions/decks.js
+++ actions/decks.js
@@ -1,6 +1,7 @@
 export const RECEIVE_DECKS = "RECEIVE_DECKS";
 export const ADD_DECK = "ADD_DECK";
 export const DELETE_DECK = "DELETE_DECK";
+export const UPDATE_DECK = "UPDATE_DECK";
 
 export function receiveDecks(decks) {
   return {
@@ -20,5 +21,12 @@ export function deleteDeck(deckId) {
   return {
     type: DELETE_DECK,
     deckId
+  };
+}
+
+export function updateDeck(deck) {
+  return {
+    type: UPDATE_DECK,
+    deck
   };
 }
