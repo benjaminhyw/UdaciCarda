77c4c45f02934f0cda5e3106bd4c508a6bfe4c45
Ben Flores <beflore@microsoft.com>
2019-03-17 20:45:14 -0700

    Create new QuizResults page

---
+++ components/QuizResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function QuizResults() {
+  return (
+    <View>
+      <Text>EMPTY QUIZ RESULT COMPONENT</Text>
+    </View>
+  );
+}
