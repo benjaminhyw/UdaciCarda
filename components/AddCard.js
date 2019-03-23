1a046e233b4075834a0578bb2400b9ed3ec397a5
Ben Flores <beflore@microsoft.com>
2019-03-23 12:47:22 -0700

    Add conditional disabling logic

--- components/AddCard.js
+++ components/AddCard.js
@@ -9,18 +9,25 @@ import {
   KeyboardAvoidingView
 } from "react-native";
 import { timeToString } from "../utils/helpers";
-import { purple, white } from "../utils/colors";
+import { purple, white, gray } from "../utils/colors";
 import { connect } from "react-redux";
 import { addCard } from "../actions/cards";
 import { submitCard } from "../utils/api";
 import { NavigationActions } from "react-navigation";
 
-function SubmitBtn({ onPress }) {
+function SubmitBtn({ onPress, disabled }) {
   return (
     <TouchableOpacity
       style={
-        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.androidSubmitbtn
+        Platform.OS === "ios"
+          ? disabled
+            ? styles.disabledIosSubmitBtn
+            : styles.iosSubmitBtn
+          : disabled
+          ? styles.disabledAndroidSubmitbtn
+          : styles.androidSubmitbtn
       }
+      disabled={disabled}
       onPress={onPress}
     >
       <Text style={styles.submitBtnText}>Submit</Text>
@@ -57,10 +64,22 @@ class AddCard extends Component {
     this.props.navigation.navigate("DeckDetail", {
       deckInformation: this.props.deckInformation
     });
+    1;
     // submitCard(card);
   };
 
   render() {
+    let isDisabled =
+      this.state.answer.toLowerCase().trim().length > 0 &&
+      (this.state.answer.toLowerCase().trim() === "true" ||
+        this.state.answer.toLowerCase().trim() === "false");
+    console.log(isDisabled);
+    console.log(this.state.answer);
+    console.log("$$$!");
+    console.log(!this.state.answer.toLowerCase().trim());
+    console.log(!this.state.answer.toLowerCase().trim());
+    console.log("$$$");
+
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
         <View style={styles.center}>
@@ -76,7 +95,7 @@ class AddCard extends Component {
             onChangeText={answer => this.setState({ answer })}
             value={this.state.answer}
           />
-          <SubmitBtn onPress={this.submit} />
+          <SubmitBtn onPress={this.submit} disabled={!isDisabled} />
         </View>
       </KeyboardAvoidingView>
     );
@@ -105,6 +124,25 @@ const styles = StyleSheet.create({
   },
   androidSubmitbtn: {
     backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  disabledIosSubmitBtn: {
+    backgroundColor: gray,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  disabledAndroidSubmitbtn: {
+    backgroundColor: gray,
     padding: 10,
     paddingLeft: 30,
     paddingRight: 30,
